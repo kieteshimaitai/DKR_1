50fe58c619247127c8b5e8104e6a42b10b391cb3
kieteshimaitai <150898995+kieteshimaitai@users.noreply.github.com>
2023-12-19 16:16:40 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿var
+  x,xm:real;
+  
+begin
+  writeln('введите x');
+  readln(x);
+  if x <-6 then
+    xm:=91-x;
+  if (x>= -6) and (x <1) then
+    xm:=x**round((1/3)*100/100)+cos(2*x);
+  if (x>=1) and (x<6) then
+    xm:=25*(-x);
+  if 6<=x then
+    xm:=65;
+  writeln(xm);
+end.
---
+++ 2.pas
@@ -0,0 +1,18 @@
+﻿var
+  x,xm:real;
+  
+begin
+  x:=-8;
+  while x<= 8 do begin
+  if x <-6 then
+    xm:=91-x;
+  if (x>= -6) and (x <1) then
+    xm:=x**round((1/3)*100/100)+cos(2*x);
+  if (x>=1) and (x<6) then
+    xm:=25*(-x);
+  if 6<=x then
+    xm:=65;
+  writeln(xm);
+  x+=0.2;
+  end
+end.
